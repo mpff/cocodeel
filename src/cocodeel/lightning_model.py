d2a6b37cc4b92e9f61bfc6edca5c4c83ab771a64
Manuel Pfeuffer <manuel.pfeuffer@hu-berlin.de>
2024-11-05 15:14:54 +0100

    Refactor CovarNeuralNetwork model and imports

--- src/cocodeel/lightning_model.py
+++ src/cocodeel/lightning_model.py
@@ -1,12 +1,12 @@
-import lightning as pl
-import torch.nn as nn
+import copy
+import torch
+import lightning
 
 
-class CovarNeuralNetwork(pl.LightningModule):
+class CovarNeuralNetwork(lightning.LightningModule):
 
     def __init__(self, backbone, output_func, loss_func,
-                 num_covars=0, backbone_params={},
-                 optimizer_params={}, scheduler_params={}):
+                 num_covars, backbone_params, optimizer_params, scheduler_params):
         """ Neural network with (or without) covariates added in the last layer.
         Parameters:
             backbone (nn.Module): The backbone model for feature extraction.
@@ -26,22 +26,24 @@ class CovarNeuralNetwork(pl.LightningModule):
         self.optimizer_params = optimizer_params
         self.scheduler_params = scheduler_params
         # Initialise last layer of the network.
-        self.deep_predictor = nn.Linear(self.num_features, 1, bias=True)
-        if self.num_covars > 0:
-            self.struct_predictor = nn.Linear(self.num_covars, 1, bias=False)
-        else:
-            self.struct_predictor = lambda X: 0
+        # IMPORTANT: No bias term, because we assume that we always have at least a covariate vector of ones!
+        self.deep_predictor = torch.nn.Linear(self.num_features, 1, bias=False)
+        self.struct_predictor = torch.nn.Linear(self.num_covars, 1, bias=False)
         # Save hyperparameters to checkpoint.
         self.save_hyperparameters("num_covars", "num_features", "optimizer_params", "scheduler_params")
 
     def forward(self, U, X):
         H = self.backbone(U)
         eta = self.deep_predictor(H) + self.struct_predictor(X)
+    def linear_predictor(self, u, x):
+        h = self.model.backbone(u)
+        return self.model.deep_predictor(h) + self.model.struct_predictor(x)
+
+        eta = self.linear_predictor(u, x)
         return self.output_func(eta)
 
-    def loss(self, U, X, y):
-        H = self.backbone(U)
-        eta = self.deep_predictor(H) + self.struct_predictor(X)
+    def loss(self, u, x, y):
+        eta = self.linear_predictor(u, x)
         return self.loss_func(eta.squeeze(), y)
 
     # PyTorch Lightning hooks
@@ -51,6 +53,6 @@ class CovarNeuralNetwork(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), **self.optimizer_params)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, **self.scheduler_params)
+        optimizer = torch.optim.AdamW(self.parameters(), **self.optimizer_params)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, **self.scheduler_params)
         return {'optimizer': optimizer, 'lr_scheduler': scheduler, 'monitor': 'val_loss'}
