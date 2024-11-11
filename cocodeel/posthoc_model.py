07804e2163f8c03ecb77c6b5fc2427cbccf6563b
Manuel Pfeuffer <manuel.pfeuffer@hu-berlin.de>
2024-11-11 16:11:54 +0100

    Add PostHocOrthogonalizedModel class and test

---
+++ cocodeel/posthoc_model.py
@@ -0,0 +1,31 @@
+import copy
+import torch
+import lightning
+
+class PostHocOrthogonalizedModel(lightning.LightningModule):
+
+    def __init__(self, model, train_dataloader):
+        """ Orthogonalizes a pre-trained model over the training data set. """
+        super().__init__()
+        self.model = copy.deepcopy(model)
+        # Update last layer with orthogonalization.
+        self.model.ortho_parameters = torch.nn.Parameter(torch.zeros((self.model.num_covars, 1)), requires_grad=False)
+        with torch.no_grad():
+            # See Rügamer (2023) ... Algorithm ...
+            xtx = torch.zeros((self.model.num_covars, self.model.num_covars), device=self.model.device)
+            xte = torch.zeros((self.model.num_covars, 1), device=self.model.device)
+            for batch in train_dataloader:
+                u = batch["image"].to(self.model.device)
+                x = batch["covar"].to(self.model.device)
+                h = self.model.backbone(u)
+                eta_h = self.model.deep_predictor(h)
+                xtx += x.T @ x
+                xte += x.T @ eta_h
+            self.model.ortho_parameters.data = torch.linalg.solve(xtx, xte)
+            self.model.struct_predictor.weight.data += self.model.ortho_parameters.T
+
+    def forward(self, u, x):
+        h = self.model.backbone(u)
+        h_orth = h - x @ self.model.ortho_parameters
+        eta = self.model.deep_predictor(h_orth) + self.model.struct_predictor(x)
+        return self.model.output_func(eta)
---
+++ tests/test_posthoc_model.py
@@ -0,0 +1,37 @@
+import unittest
+import torch
+
+from tests.utils.model import TestCaseBackbone
+
+from cocodeel.dataset import CovarDataset
+from cocodeel.model import CovarNeuralNetwork
+from cocodeel.posthoc_model import PostHocOrthogonalizedModel
+
+
+class TestPostHocOrthoganlizedModel(unittest.TestCase):
+    def setUp(self):
+        params = {"backbone": TestCaseBackbone,
+                  "loss_func": torch.nn.MSELoss,
+                  "output_func": torch.nn.Identity,
+                  "num_features": 32,
+                  "num_covars": 2}
+        self.size = 4
+        self.pretrained_net = CovarNeuralNetwork(**params)
+        for param in self.pretrained_net.parameters():
+            param.data = torch.nn.parameter.Parameter(torch.ones_like(param))
+        # Create dataset.
+        self.train_image = torch.randn(self.size, 32)
+        self.train_covar = torch.randn(self.size, 2)
+        self.train_label = torch.randn(self.size, 1)
+        self.train_dataset = CovarDataset(self.train_image, self.train_covar, self.train_label)
+        self.train_loader = torch.utils.data.DataLoader(dataset=self.train_dataset)
+
+    @torch.no_grad()
+    def test_train_pho_model(self):
+        model = PostHocOrthogonalizedModel(self.pretrained_net, self.train_loader)
+        output = model(self.train_image, self.train_covar)
+        self.assertEqual((self.size, 1), output.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
