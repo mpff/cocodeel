98a13dc0028fa6b9ad5ee0981d565c53af3c2cfc
Manuel Pfeuffer <manuel.pfeuffer@hu-berlin.de>
2025-09-30 16:13:46 +0200

    Clean repository

--- tests/utils/model.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-class TestCaseBackbone(torch.nn.Module):
-    def __init__(self, num_features=16):
-        super().__init__()
-        self.num_features = num_features
-
-    def forward(self, x):
-        return x
