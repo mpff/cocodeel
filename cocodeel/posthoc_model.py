51bd2cdeea3b7e0955a781b3a192af956334db16
Manuel Pfeuffer <manuel.pfeuffer@hu-berlin.de>
2025-10-22 15:59:23 +0200

    Add Pytorch Refit after glmnet refit. Hack but works perfectly.

--- cocodeel/posthoc_model.py
+++ cocodeel/posthoc_model.py
@@ -110,18 +110,19 @@ class PostHocCovarNetwork(BaseNetwork):
         # Prepare numpy arrays
         X_np = torch.cat([X, Z], dim=1).cpu().numpy()
         y_np = y.cpu().numpy()
-        #penalty_factor = np.ones(X_np.shape[1])  # all penalized equally (can be customized!)
+        p_fac_np = np.ones(X_np.shape[1])
+        #p_fac_np[X.shape[1]:] = 0.0  # No penalty on covariates
 
         with localconverter(ro.default_converter + numpy2ri.converter):
             X_r = ro.conversion.py2rpy(X_np)
             y_r = ro.conversion.py2rpy(y_np)
-            #p_fac_r = ro.conversion.py2rpy(penalty_factor)
+            p_fac_r = ro.conversion.py2rpy(p_fac_np)
 
         if lam is not None:
             fit = glmnet.glmnet(
                 X_r, y_r, alpha=0,
                 lambda_=ro.FloatVector([lam]),
-                #penalty_factor=p_fac_r,
+                penalty_factor=p_fac_r,
                 intercept=False
             )
             coefs = ro.r["as.matrix"](glmnet.coef_glmnet(fit, s=lam))
@@ -129,7 +130,7 @@ class PostHocCovarNetwork(BaseNetwork):
         else:
             cv_fit = glmnet.cv_glmnet(
                 X_r, y_r, alpha=0,
-                #penalty_factor=p_fac_r,
+                penalty_factor=p_fac_r,
                 intercept=False
             )
             coefs = ro.r["as.matrix"](glmnet.coef_cv_glmnet(cv_fit, s="lambda.min"))
@@ -137,11 +138,49 @@ class PostHocCovarNetwork(BaseNetwork):
 
         coefs_np = np.array(coefs).flatten()
         n_feat = X.shape[1]
-
+        
         # Update model parameters
         self.fx.weight.copy_(torch.tensor(coefs_np[1:n_feat + 1], dtype=torch.float32))
         self.fz.weight.copy_(torch.tensor(coefs_np[n_feat + 1:], dtype=torch.float32))
+        
+        # Store best Lambda
         self.lam.copy_(torch.tensor(best_lambda, dtype=torch.float32))
+        
+        # TODO: This is debug code to refit in Pytorch! REMOVE LATER!
+        print("Refitting linear effects in Pytorch for debugging...")
+        # 1. Regress y_tilde on Z to get residuals.
+        resid_y = y - Z @ torch.linalg.lstsq(Z, y).solution
+        # 2. Regress X on Z to get residuals.
+        resid_X = X - Z @ torch.linalg.lstsq(Z, X).solution
+        # 3. Fit fx on residuals.
+        if self.lam == 0.0:
+            beta_X = torch.linalg.lstsq(resid_X, resid_y).solution
+        else:
+            I = torch.eye(resid_X.shape[1], device=resid_X.device)
+            beta_X = torch.linalg.solve(
+                resid_X.T @ resid_X + self.lam * I,
+                resid_X.T @ resid_y
+            )
+
+        # Estimate fz weights by LS fit on new residuals:
+        # 1. Remove fx from y.
+        resid_y = y - X @ beta_X
+        # 2. Fit fz on residuals.
+        if self.lam == 0.0:
+            beta_Z = torch.linalg.lstsq(Z, resid_y).solution
+        else:
+            I = torch.eye(Z.shape[1], device=Z.device)
+            beta_Z = torch.linalg.solve(
+                Z.T @ Z + self.lam * I,
+                Z.T @ resid_y
+            )
+
+        # Update weights.
+        self.fx.weight.data.copy_(beta_X.view(1, -1))
+        self.fz.weight.data.copy_(beta_Z.view(1, -1))
+
+        
+
 
     @torch.no_grad()
     def _fit_orthogonalization(self, dataloader):
